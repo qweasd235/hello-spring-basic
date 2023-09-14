01d3f27c0ee01a09064f55ba559313027f933d27
qweasd235 <235qwerty@naver.com>
2023-09-15 00:03:31 +0900

    spring-입문편 9/14 공부 - 자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
     // new 사용 시 인스턴스 생성하는 것으로 DI로 하여 작업하는 것이 효율적
+    // DI는 필드 주입, 생성자 주입, setter 주입 방법 등이 있다.
+    // 필드 주입은 설정 시 변경할 수 없기 때문에 비추
+    // setter 주입 단점은 public으로 노출되어 아무데서나 호출되도록 되어 문제가 됨
     private final MemberService memberService;
 
     @Autowired
